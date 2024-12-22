73e4edca1faf7b67fca04f1da1642a2153a66c22
Vishesh Tripathi <tripathiishu467@gmail.com>
2024-12-22 19:04:03 +0530

    dvjskjv

--- client/src/pages/Team/Team.jsx
+++ client/src/pages/Team/Team.jsx
@@ -14,34 +14,46 @@ const TeamPage = () => {
       <div className="team-container">
         {/* Content */}
         <div className="content">
-          <div className="team-members">
+          <div className="team-membersmentor">
             {/* Jai Prakash Saini (Vice Chancellor) - Top Center */}
             <div className="member-card larger-card">
-              <img src={vc} alt="Jai Prakash Saini" className="member-image larger-image" />
-              <p className="member-name">Jai Prakash Saini</p>
+              <img src={vc} alt="Jai Prakash Saini" className="member-imagementor larger-image" />
+              <p className="member-namementor">Jai Prakash Saini</p>
               <p className="member-position">Hon'ble Vice Chancellor</p>
             </div>
           </div>
 
-          <div className="team-members row">
+          <div className="team-membersmentor row">
             {/* Rajan Mishra */}
             <div className="member-card">
-              <img src={rajan} alt="Rajan Mishra" className="member-image" />
-              <p className="member-name">Rajan Mishra</p>
+              <img src={rajan} alt="Rajan Mishra" className="member-imagementor" />
+              <p className="member-namementor">Rajan Mishra</p>
               <p className="member-position">Coordinator</p>
             </div>
 
             {/* Anupam Sahu */}
             <div className="member-card">
-              <img src={sahu} alt="Anupam Sahu" className="member-image" />
-              <p className="member-name">Anupam Sahu</p>
+              <img src={sahu} alt="Anupam Sahu" className="member-imagementor" />
+              <p className="member-namementor">Anupam Sahu</p>
               <p className="member-position">Member</p>
             </div>
 
             {/* New Member */}
             <div className="member-card">
-              <img src={rajan} alt="New Member" className="member-image" />
-              <p className="member-name">New Member</p>
+              <img src={rajan} alt="New Member" className="member-imagementor" />
+              <p className="member-namementor">New Member</p>
+              <p className="member-position">New Position</p>
+            </div>
+             {/* New Member */}
+             <div className="member-card">
+              <img src={rajan} alt="New Member" className="member-imagementor" />
+              <p className="member-namementor">New Member</p>
+              <p className="member-position">New Position</p>
+            </div>
+             {/* New Member */}
+             <div className="member-card">
+              <img src={rajan} alt="New Member" className="member-imagementor" />
+              <p className="member-namementor">New Member</p>
               <p className="member-position">New Position</p>
             </div>
           </div>
